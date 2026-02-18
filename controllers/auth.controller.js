e620633bf4d4d8cd5ba780350f913661b47a7137
pi8878 <simangolwalifwatila99@gmail.com>
2026-02-18 16:10:29 -0500

    Create auth controller logic for sign up function

---
+++ controllers/auth.controller.js
@@ -0,0 +1,65 @@
+// forms the logic of the application and also interact with the database. 
+// They receive requests from the routes and send responses back to the client.
+
+import mongoose from "mongoose"
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from "../models/user.model.js";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
+
+// what is a re body? req.body is an object containing the data sent by the client in the request body.
+// It is typically used in POST and PUT requests to send data to the server. 
+// For example, when a user submits a form, the data from the form is sent in the request body 
+// and can be accessed using req.body in the controller function that handles the request.
+
+export const signUp = async(req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        // logic to create a new user
+        const {name, email, password} = req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            const error = new Error('User with this email already exists');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create([{name, email, password: hashedPassword}], {session});
+
+        const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            data: {
+                token,
+                user: newUsers[0],
+            }
+        })
+        
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
+}
+
+
+
+export const signIn = async(req, res, next) => {}
+
+export const signOut = async(req, res, next) => {}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,16 @@
+// Routes define the endpoints of the application and also specify which controller function will handle the request for each endpoint. 
+// They receive requests from the client and send them to the appropriate controller function for processing.
+
+// import { sign } from 'crypto';
+
 import {Router} from 'express';
+
+import { signIn, signOut, signUp } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({body: {title: 'Sign up'}}));
-authRouter.post('/sign-in', (req, res) => res.send({body: {title: 'Sign in'}}));
-authRouter.post('/sign-out', (req, res) => res.send({body: {title: 'Sign out'}}));
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out',  signOut);
 
 export default authRouter;
