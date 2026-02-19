675fe177d74c375d2524fbdcaeffc12ede96ee51
pi8878 <simangolwalifwatila99@gmail.com>
2026-02-18 19:00:12 -0500

    create user controller with getUsers and getUser function as well as user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,16 @@
 import {Router} from 'express';
+import { getUser, getUsers } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({body: {title: 'Get all users'}}));
-userRouter.get('/:id', (req, res) => res.send({body: {title: 'Get user by ID'}}));
+userRouter.get('/', getUsers);
+
+userRouter.get('/:id', getUser);
+
 userRouter.post('/', (req, res) => res.send({body: {title: 'Create new user'}}));
+
 userRouter.put('/:id', (req, res) => res.send({body: {title: 'Update user by ID'}}));
+
 userRouter.delete('/:id', (req, res) => res.send({body: {title: 'Delete user by ID'}}));
 
 export default userRouter;
